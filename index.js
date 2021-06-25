ae031d9b8aa235e2cb7e36a33aa4f04b456de1b7
Bruno Zablotny <rusholdfield@gmail.com>
2021-06-26 01:05:59 +0200

    Testing

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const bodyParser = require('body-parser');
-
 const express = require('express'),
   morgan = require('morgan'),
   swaggerUI = require("swagger-ui-express"),
@@ -31,8 +30,6 @@ mongoose.connect("mongodb+srv://admin:pomidor@cluster0.8x8mn.mongodb.net/myFlixD
   useUnifiedTopology: true
 });
 
-const port = process.env.port || 8080;
-
 const options = {
   definition: {
     openapi: "3.0.0",
@@ -53,14 +50,17 @@ app.use(cors());
 
 let auth = require('./routes/auth.js')(app);
 
-app.use("/", swaggerUI.serve, swaggerUI.setup(specs));
-
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use("/movies", moviesRouter);
 app.use("/genres", genresRouter);
 app.use("/directors", directorsRouter);
 app.use("/users", usersRouter);
 
-app.listen(port, () => console.log(`The server is running on port ${port}`));
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
+});
