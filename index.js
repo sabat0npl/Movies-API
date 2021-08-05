c8c24e1a4668a918a98098843325479a0ad51e18
Bruno Zablotny <rusholdfield@gmail.com>
2021-08-05 02:30:02 +0200

    Switching off cors

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const express = require('express'),
 
 require('./js/passport.js');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://brunoza-flix.netlify.app'];
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://brunoza-flix.netlify.app'];
+
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true
@@ -47,16 +48,16 @@ const specs = swaggerJsDoc(options);
 
 const app = express();
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors(
+    // { origin: (origin, callback) => {
+  //   if (!origin) return callback(null, true);
+  //   if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+  //     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+  //     return callback(new Error(message), false);
+  //   }
+  //   return callback(null, true);
+  // }}
+  ));
 
 let auth = require('./routes/auth.js')(app);
 
